a6e255ecfcf6387ffe5825f939d4028d17cb85b2
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-09-06 12:09:38 +0300

    Complete Fetch Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React from "react";
+import { useState , useEffect } from "react";
+
+function App(){
+    const [image,setImage] = useState([])
+    const [isLoaded,setIsLoaded] = useState(false)
+
+    const imageRequest = ()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(image => {console.log(image.message)
+            setImage(image.message);
+            setIsLoaded(true);
+        })
+    }
+    useEffect(
+        ()=>imageRequest(),
+        []
+    )
+
+  return (
+    <div>
+        {isLoaded ? <img src={image} alt="A Random Dog"/> : <p> Loading... </p>}
+    </div>
+  )  
+}
+
+export default App
